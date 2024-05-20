94699958a02784e2eb3853cf19be7fe707af4f0c
jekakoba <endlesss2k2k@gmail.com>
2024-05-20 15:41:05 +0300

    test form 15

--- config/webpack.dev.js
+++ config/webpack.dev.js
@@ -53,7 +53,7 @@ const config = {
 		compress: true,
 		port: 'auto',
 		hot: true,
-		host: 'localhost', // localhost
+		host: 'local-ip', // localhost
 		//В режимі розробника папка 
 		// результатом (dist) буде створюватися на диску)
 		//devMiddleware: {
--- src/js/files/axios.js
+++ src/js/files/axios.js
@@ -1,113 +1,79 @@
-import { flsModules } from "./modules.js"
-import axios from "axios"
+import axios from "axios";
 
 document.addEventListener('DOMContentLoaded', () => {
-	const forms = document.querySelectorAll('.form')
-	if (forms.length === 0) return
+	const forms = document.querySelectorAll('.form');
+	if (forms.length === 0) return;
+
 	forms.forEach(form => {
-		// Перша форма
-		const firstFormTitle = form.querySelector('[data-first-form-title]')
-		const firstFormName = form.querySelector('[data-first-form-name]')
-		const firstFormEmail = form.querySelector('[data-first-form-email]')
-		const firstFormMessage = form.querySelector('[data-first-form-message]')
-		const firstFormSend = form.querySelector('[data-first-form-send]')
+		const formTitleInput = form.querySelector('[data-form-title]');
+		const nameInput = form.querySelector('[data-form-name]');
+		const emailInput = form.querySelector('[data-form-email]');
+		const messageInput = form.querySelector('[data-form-message]');
+		const formSendButton = form.querySelector('[data-form-send]');
 
-		// Друга  форма
-		const secondFormTitle = form.querySelector('[data-second-form-title]')
-		const secondFormName = form.querySelector('[data-second-form-name]')
-		const secondFormEmail = form.querySelector('[data-second-form-email]')
-		const secondFormMessage = form.querySelector('[data-second-form-message]')
-		const secondFormSend = form.querySelector('[data-second-form-send]')
+		let formData = {};
 
+		const requiredMask = /^\w+([.-]?\w+)@\w+([.-]?\w+)(.\w{2,8})+$/;
 
-		// Валідація email
-		const requiredMask = /^\w+([.-]?\w+)@\w+([.-]?\w+)(.\w{2,8})+$/
-
-		// Масиви форм, куди йдуть зібрані дані
-		let dataFirstForm = []
-		let dataSecondForm = []
-
-
-		// Валідація форми
-		function validationFirstForm() {
-			if (firstFormName && firstFormEmail && firstFormMessage) {
-				const userFirstNameValue = firstFormName.value;
-				const userFirstEmailValue = firstFormEmail.value;
-				const userFirstMessageValue = firstFormMessage.value;
-				const subjectFirstForm = firstFormTitle ? firstFormTitle.value : '';
-				toggleErrorClass(firstFormName, userFirstNameValue.length === 0);
-				toggleErrorClass(firstFormEmail, userFirstEmailValue.length === 0 || !validateEmail(userFirstEmailValue));
-				toggleErrorClass(firstFormMessage, userFirstMessageValue.length === 0);
-				if (userFirstNameValue.length > 0 && userFirstMessageValue.length > 0 && validateEmail(userFirstEmailValue)) {
-					dataFirstForm['name'] = userFirstNameValue;
-					dataFirstForm['email'] = userFirstEmailValue;
-					dataFirstForm['message'] = userFirstMessageValue;
-					dataFirstForm['mail-title'] = subjectFirstForm;
-					getMailTitle(dataFirstForm);
-					chatSubmit(dataFirstForm, [firstFormName, firstFormEmail, firstFormMessage]);
-				}
-			}
-		}
-		function validationSecondForm() {
-			if (secondFormName && secondFormEmail && secondFormMessage) {
-				const userSecondNameValue = secondFormName.value;
-				const userSecondEmailValue = secondFormEmail.value;
-				const userSecondMessageValue = secondFormMessage.value;
-				const subjectSecondForm = secondFormTitle ? secondFormTitle.value : '';
-
-				toggleErrorClass(secondFormName, userSecondNameValue.length === 0);
-				toggleErrorClass(secondFormEmail, userSecondEmailValue.length === 0 || !validateEmail(userSecondEmailValue));
-				toggleErrorClass(secondFormMessage, userSecondMessageValue.length === 0);
-				if (userSecondNameValue.length > 0 && userSecondMessageValue.length > 0 && validateEmail(userSecondEmailValue)) {
-					dataSecondForm['name'] = userSecondNameValue;
-					dataSecondForm['email'] = userSecondEmailValue;
-					dataSecondForm['message'] = userSecondMessageValue;
-					dataSecondForm['mail-title'] = subjectSecondForm;
-					getMailTitle(dataSecondForm);
-					chatSubmit(dataSecondForm, [secondFormName, secondFormEmail, secondFormMessage]);
-				}
-			}
-		}
-
-		// Заголовок сторінки
-		function getMailTitle(targetArray) {
-			const emailSubject = document.querySelector('#subject')
-			const emailSubjectValue = emailSubject.value
-			targetArray['subject'] = emailSubjectValue
-		}
-		// Валідація Email
 		function validateEmail(email) {
-			return requiredMask.test(email)
+			return requiredMask.test(email);
 		}
-		// Додавання класу  _form-error для батька інпута 
+
 		function toggleErrorClass(element, condition) {
-			if (condition) { element.parentElement.classList.add('_form-error') }
-			else { element.parentElement.classList.remove('_form-error') }
+			if (condition) {
+				element.parentElement.classList.add('_form-error');
+			} else {
+				element.parentElement.classList.remove('_form-error');
+			}
 		}
 
-		// Відправка форми
+		function getMailTitle(targetArray) {
+			const emailSubject = document.querySelector('#subject');
+			const emailSubjectValue = emailSubject.value;
+			targetArray['subject'] = emailSubjectValue;
+		}
+
 		async function chatSubmit(currentData, inputsArr) {
-			const formData = new FormData()
+			const formData = new FormData();
 			for (const [key, value] of Object.entries(currentData)) {
-				formData.append(key, value)
+				formData.append(key, value);
 			}
-			form ? form.classList.add('_sending') : null
+			form ? form.classList.add('_sending') : null;
 			try {
-				const response = await axios.post('../files/sendmail/sendmail.php', formData)
+				const response = await axios.post('../files/sendmail/sendmail.php', formData);
 				for (let input of inputsArr) {
-					input.value = ''
+					input.value = '';
 				}
-				form ? form.classList.remove('_sending') : null
-				const inputsActive = document.querySelectorAll('._input-active')
-				if (inputsActive.length > 0) inputsActive.forEach(input => input.classList.remove('_input-active'))
+				form ? form.classList.remove('_sending') : null;
+				const inputsActive = document.querySelectorAll('._input-active');
+				if (inputsActive.length > 0) inputsActive.forEach(input => input.classList.remove('_input-active'));
 			} catch (error) {
-				form ? form.classList.remove('_sending') : null
+				form ? form.classList.remove('_sending') : null;
 			}
 		}
-		if (firstFormSend) firstFormSend.addEventListener("click", validationFirstForm);
-		if (secondFormSend) secondFormSend.addEventListener("click", validationSecondForm);
+
+		function validateForm() {
+			const userNameValue = nameInput.value;
+			const userEmailValue = emailInput.value;
+			const userMessageValue = messageInput.value;
+			const subject = formTitleInput ? formTitleInput.value : '';
+
+			toggleErrorClass(nameInput, userNameValue.length === 0);
+			toggleErrorClass(emailInput, userEmailValue.length === 0 || !validateEmail(userEmailValue));
+			toggleErrorClass(messageInput, userMessageValue.length === 0);
+
+			if (userNameValue.length > 0 && userMessageValue.length > 0 && validateEmail(userEmailValue)) {
+				formData['name'] = userNameValue;
+				formData['email'] = userEmailValue;
+				formData['message'] = userMessageValue;
+				formData['mail-title'] = subject;
+				getMailTitle(formData);
+				chatSubmit(formData, [nameInput, emailInput, messageInput]);
+			}
+		}
+
+		if (formSendButton) {
+			formSendButton.addEventListener("click", validateForm);
+		}
 	});
-})
-
-
-
+});
